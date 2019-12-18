eac4074a447ecf08eab62301a9076d1a7e7570c3
Riccardo Consiglio <47518595+rc2003@users.noreply.github.com>
2019-12-18 10:31:15 +0100

    Update d-.cpp

--- d-.cpp
+++ d-.cpp
@@ -1,81 +1,109 @@
 #include <iostream>
 using namespace std;
-float USD_EUR(float tasso, float USD, float EUR)
-	EUR = tasso*USD;
-	cout << EUR;
-float EUR_USD(float tasso, float EUR, float USD)
-	USD = tasso/EUR;
-	cout << USD;
-float EUR_GBP(float tasso, float EUR, float GBP)
-	GBP = tasso/EUR;
-	cout << GBP;
-float GBP_EUR(float tasso, float GBP, float EUR)
-	EUR = tasso*GBP;
-	cout << EUR;
-float tassoEUR_USD(float tasso, float EUR, float USD)
-	tasso = EUR/USD;
-	cout << tasso;
-float tassoEUR_GBP(float tasso, float EUR, float GBP)
-	tasso = EUR/GBP;
-	cout << tasso;
+
+
+float dollari_euro (float tasso, float valuta1, float valuta2){
 	
-int main(){
-	int scelta;
-	int importo;
-	cout << "1. Cambio DOLLARI\EURO" << endl;
-	cout << "2. Cambio EURO\DOLLARI" << endl;
-	cout << "3. Cambio EURO\STERLINE" << endl;
-	cout << "4. Cambio STERLINE\EURO" << endl;
-	cout << "5. Variazione tasso EUR\USD" << endl;
-	cout << "6. Variazione tasso EUR\GBP" << endl;
-	cout << "7. Esci" << endl;
-	cin >> scelta;
-	
-	switch(scelta){
-	case(1){
-		cout << "Inserisci l'importo -> ";
-		cin << importo;
-		EUR_USD(1.1, importo);
-		break;
-	}
-	case(2){
-		cout << "Inserisci l'importo -> ";
-		cin << importo;
-		USD_EUR(1.1, importo);
-		break;
-	}
-	case(3){
-		cout << "Inserisci l'importo -> ";
-		cin << importo;
-		EUR_GBP(0.775, importo);
-		break;
-	}
-	case(4){
-		cout << "Inserisci l'importo -> ";
-		cin << importo;
-		GBP_EUR(0.775, importo);
-		break;
-	}
-	case(5){
-		cout << "Inserisci l'importo in euro -> ";
-		cin << importo;
-		tassoEUR_USD(1.1, importo);
-		break;
-	}
-	case(6){
-		cout << "Inserisci l'importo -> ";
-		cin << importo;
-		EUR_USD(1.1, importo);
-		break;
-	}
-	case(7){
-		cout << "Inserisci l'importo -> ";
-		cin << importo;
-		EUR_USD(1.1, importo)
-		break;
-	}
-	}
+	valuta2= valuta1*tasso;
+	cout<<"La tua valuta in euro e' "<<valuta2<<endl;
 	
 	
 }
+float euro_dollari (float tasso, float valuta1, float valuta2){
 	
+	valuta2= valuta1/tasso;
+	cout<<"La tua valuta in dollari e' "<<valuta2<<endl;
+	
+	
+}
+float euro_sterline (float tasso, float valuta1, float valuta2){
+	
+	valuta2= valuta1/tasso;
+	cout<<"La tua valuta in sterline e' "<<valuta2<<endl;
+	
+	
+}
+float sterline_euro (float tasso, float valuta1, float valuta2){
+	
+	valuta2= valuta1*tasso;
+	cout<<"La tua valuta in euro e' "<<valuta2<<endl;
+	
+	
+}
+float tasso1 (float tasso){
+	
+	
+	cout<<"Il tasso USD/EUR e' "<<tasso<<endl;
+	
+	
+}
+float tasso2 (float tasso){
+	
+	
+	cout<<"Il tasso GBP/EUR e' "<<tasso<<endl;
+	
+	
+}
+
+
+
+int main(){
+	int scelta;
+	float tasso, valuta1, valuta2;
+	
+	do{
+	
+		cout << "\n1. Cambio DOLLARI/EURO" << endl;
+		cout << "2. Cambio EURO/DOLLARI" << endl;
+		cout << "3. Cambio EURO/STERLINE" << endl;
+		cout << "4. Cambio STERLINE/EURO" << endl;
+		cout << "5. Variazione tasso EUR7USD" << endl;
+		cout << "6. Variazione tasso EUR/GBP" << endl;
+		cout << "7. Esci" << endl;
+		cin >> scelta;
+		
+		switch(scelta){
+			case(1):
+			{
+				cout<<"Inserisci la valuta da cambiare ";
+				cin>>valuta1;
+				cout<<"Inserisci tasso ";
+				cin>>tasso;
+				dollari_euro(tasso, valuta1, valuta2=0);
+				
+				break;
+			}
+			case(2):
+			{
+				cout<<"Inserisci la valuta da cambiare ";
+				cin>>valuta1;
+				cout<<"Inserisci tasso ";
+				cin>>tasso;
+				euro_dollari(tasso, valuta1, valuta2=0);
+				
+				break;
+			}
+			case(3):
+			{
+				cout<<"Inserisci la valuta da cambiare ";
+				cin>>valuta1;
+				cout<<"Inserisci tasso ";
+				cin>>tasso;
+				euro_dollari(tasso, valuta1, valuta2=0);
+				
+				break;
+			}
+			
+		}
+	
+	
+	
+	
+	
+	
+	}while( scelta !=7 );
+	
+
+	
+	
+}
